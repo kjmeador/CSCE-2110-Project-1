92ab7de52c95a7702f4368ec761c5a611bd86b6b
kjmeador <kolestovall@gmail.com>
2026-09-20 15:13:29 -0500

    Implement ReservationList class for reservation management
    Added ReservationList class to manage reservations using a linked list.

--- include/ReservationManager.h
+++ include/ReservationManager.h
@@ -1,1 +1,18 @@
+#include "Reservation.h"
+#include <string>
+using namespace std;
 
+//Create a Linked List Class to store reservations
+class ReservationList
+{
+private:
+    Reservation* head;
+
+public:
+    ReservationList();
+
+    void insert(int id, int studentID, string studentName, int resourceID, string date);
+    void remove(int reservationID);
+    void search(int reservationID);
+    void print(); 
+};
