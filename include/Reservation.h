32979e7d4c4fc3a8670dd829c9a358a05afd00a6
kjmeador <kolestovall@gmail.com>
2026-09-20 14:59:24 -0500

    Create Reservation class with properties and methods
    Added Reservation class with constructors and getter methods.

--- include/Reservation.h
+++ include/Reservation.h
@@ -1,1 +1,24 @@
+#include <string>
+using namespace std;
 
+//Create the Reservation Class
+class Reservation {
+private:
+    int id;
+    int studentId;
+    string studentName;
+    int resourceId;
+    string date;
+
+public:
+    //Default constructor and initialized constructor
+    Reservation();
+    Reservation(int id, int studentId, string studentName, int resourceId, string date);
+
+    //Methods to return data from class
+    int getId();
+    int getStudentId();
+    string getStudentName();
+    int getResourceId();
+    string getDate();
+};
