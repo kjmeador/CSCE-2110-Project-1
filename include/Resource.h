02a570b339c8f1bbe8af6f779e71a787d1b4edc5
Cameron West <cameronwest4@my.unt.edu>
2026-09-20 23:46:44 -0500

    Update Resource.h

--- include/Resource.h
+++ include/Resource.h
@@ -1,1 +1,35 @@
+#ifndef RESOURCE_H
+#define RESOURCE_H
 
+#include <string>
+#include <vector>
+
+using namespace std;
+
+class Resource
+{
+private:
+    string resourceID;
+    string resourceName;
+    string resourceType;
+    bool available;
+
+public:
+    Resource();
+    Resource(string id, string name, string type, bool availability);
+
+    string getResourceID() const;
+    string getResourceName() const;
+    string getResourceType() const;
+    bool getAvailability() const;
+
+    void setAvailability(bool availability);
+};
+
+vector<Resource> loadResources(string filename);
+
+Resource* searchResource(vector<Resource>& resources, string resourceID);
+
+void sortResources(vector<Resource>& resources, int criteria);
+
+#endif
