1b95508e5e73d5e4ccd04c6fc39252ed0f9aa5b4
kjmeador <kolestovall@gmail.com>
2026-09-20 19:16:37 -0500

    Implement initial Reservation class with constructors and getters

--- src/Reservation.cpp
+++ src/Reservation.cpp
@@ -1,1 +1,33 @@
+#include "Reservation.h"
+#include <string>
 
+using namespace std;
+
+Reservation::Reservation()
+    : id(0), studentId(0), studentName(""),
+      resourceId(0), date(""), next(NULL) {}
+
+Reservation::Reservation(int id, int studentId, string studentName,
+                         int resourceId, string date)
+    : id(id), studentId(studentId), studentName(studentName),
+      resourceId(resourceId), date(date), next(NULL) {}
+
+int Reservation::getId() {
+    return id;
+}
+
+int Reservation::getStudentId() {
+    return studentId;
+}
+
+string Reservation::getStudentName() {
+    return studentName;
+}
+
+int Reservation::getResourceId() {
+    return resourceId;
+}
+
+string Reservation::getDate() {
+    return date;
+}
